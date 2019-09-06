99f39afc17cb782f39f6dd58c0af0874700eb133
Ben Serrette <ben.serrette@outlook.com>
2019-09-05 22:50:25 -0400

    Adds skills.

---
+++ src/database/skills.js
@@ -0,0 +1,112 @@
+export default {
+    Perception: {
+        stat: "INT",
+        name: "Perception",
+        description: "Search for the Hidden; Detect Lies, Emotions"
+    },
+    Tracking: {
+        stat: "INT",
+        name: "Tracking",
+        description: "Follow a Trail Left Behind"
+    },
+    Education: {
+        stat: "INT",
+        name: "Education",
+        description: "Know Sciences, History, Trivia, Current Events"
+    },
+    "Local Expert": {
+        stat: "INT",
+        name: "Local Expert",
+        description: "Know Local Area, Local Factions, Agendas —"
+    },
+    Interface: {
+        stat: "NET",
+        name: "Interface",
+        description: "Special “hacker” Skill available only to Netrunners"
+    },
+    Marksmanship: {
+        stat: "REF",
+        name: "Marksmanship",
+        description: "Fire Ranged Weapons Accurately"
+    },
+    Driving: {
+        stat: "REF",
+        name: "Driving",
+        description: "Drive Vehicles Skillfully"
+    },
+    Evasion: {
+        stat: "DEX",
+        name: "Evasion",
+        description: "Dodge Attacks You See Coming"
+    },
+    Athletics: {
+        stat: "DEX",
+        name: "Athletics",
+        description: "Feats of Strength, General Athleticism."
+    },
+    Stealth: {
+        stat: "DEX",
+        name: "Stealth",
+        description: "Hiding and Moving Silently"
+    },
+    Brawling: {
+        stat: "DEX",
+        name: "Brawling",
+        description: "Martial Fighting Using The Whole Body"
+    },
+    Melee: {
+        stat: "DEX",
+        name: "Melee",
+        description: "Weapon Fighting With Melee Weapons"
+    },
+    "Basic Tech": {
+        stat: "TECH",
+        name: "Basic Tech",
+        description: "Identify, Understand, and Repair Electronics"
+    },
+    Cybertech: {
+        stat: "TECH",
+        name: "Cybertech",
+        description: "Identify, Understand, and Repair Cybernetics"
+    },
+    "First Aid": {
+        stat: "TECH",
+        name: "First Aid",
+        description: "Patch Up Others and Yourself"
+    },
+    Bribery: {
+        stat: "COOL",
+        name: "Bribery",
+        description: "Know When, Who and How Much to Bribe"
+    },
+    Interrogation: {
+        stat: "COOL",
+        name: "Interrogation",
+        description: "Forcibly Extract Info from People"
+    },
+    Persuasion: {
+        stat: "COOL",
+        name: "Persuasion",
+        description: "Convince, Persuade or Influence People"
+    },
+    Concentration: {
+        stat: "WILL",
+        name: "Concentration",
+        description: "Focus, Memory, Mental Toughness"
+    },
+    Conversation: {
+        stat: "EMP",
+        name: "Conversation",
+        description: "Extract Info Through Careful Conversation"
+    },
+    "Human Perception": {
+        stat: "EMP",
+        name: "Human Perception",
+        description: "Read Faces, Bodies to Detect Lies, Emotions"
+    },
+    "Play Instrument": {
+        stat: "EMP",
+        name: "Play Instrument",
+        description: "Playing an Instrument and Writing Music"
+    }
+};
